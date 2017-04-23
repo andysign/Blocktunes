9ec5bde0c494f67c8c044ab7fb6e815eac99586c
andysign <andysignerdesigner@gmail.com>
2017-04-23 01:22:18 +0100

    Added ownable

---
+++ contracts/Ownable.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.2;
+
+// check if the caller is the owner of the contract
+contract Ownable {
+  address public owner;
+
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    if (msg.sender != owner) {
+      throw;
+    }
+    _;
+  }
+
+  function transferOwnership(address newOwner) onlyOwner {
+    if (newOwner != address(0)) {
+      owner = newOwner;
+    }
+  }
+
+}
