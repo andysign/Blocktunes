ba1b5413c52bca8aee6f69d508c7685cd8e91f74
andysign <andysignerdesigner@gmail.com>
2017-04-23 01:22:03 +0100

    Removed ownable because we can import it

--- contracts/RegisterLicenses.sol
+++ contracts/RegisterLicenses.sol
@@ -1,27 +1,6 @@
 pragma solidity ^0.4.2;
 
-// check if the caller is the owner of the contract
-contract Ownable {
-  address public owner;
-
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    if (msg.sender != owner) {
-      throw;
-    }
-    _;
-  }
-
-  function transferOwnership(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
-    }
-  }
-
-}
+import "Ownable.sol";
 
 // The main registration smart contract
 contract RegisterLicenses is Ownable {
